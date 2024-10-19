914cae9cc1964c1ea132c5ff8d0a643c5bd72451
Victor Lyuboslavsky <victor@fleetdm.com>
2024-10-19 13:15:38 -0500

    My action

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+import { readFileSync } from 'fs'
 
 /**
  * The main function for the action.
@@ -7,18 +8,41 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    // Get the PR number from the payload
+    const prNumber = github.context.payload.pull_request?.number
+    if (!prNumber) {
+      return
+    }
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Find the required reviewers.
+    const reviewer = readFileSync('REVIEWERS', 'utf8').trim()
+    if (!reviewer) {
+      core.setFailed('No reviewers found in REVIEWERS file')
+      return
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Get all reviews for this PR.
+    const githubToken = core.getInput('github-token')
+    const octokit = github.getOctokit(githubToken)
+    const reviews = await octokit.rest.pulls.listReviews({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Check if the required reviewer has approved the PR.
+    let approved = false
+    reviews.data.forEach(review => {
+      if (review.user?.login === reviewer && review.state === 'APPROVED') {
+        approved = true
+      }
+    })
+
+    // Fail the workflow run if the required reviewer has not approved the PR
+    if (!approved) {
+      core.setFailed(`${reviewer} has not approved this PR`)
+      return
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
